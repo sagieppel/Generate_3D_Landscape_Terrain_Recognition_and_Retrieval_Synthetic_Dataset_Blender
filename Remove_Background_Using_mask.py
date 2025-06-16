43f50f9a1855f173204caa84b785c9bf5d435e37
sagieppel <sagieppel@gmail.com>
2025-06-16 23:42:59 +0300

    Update Remove_Background_Using_mask.py

--- Remove_Background_Using_mask.py
+++ Remove_Background_Using_mask.py
@@ -1,9 +1,10 @@
-# Assuming each jpg image have corresponding map with _MASK.png ending use the _MASK.png file to delete/blackended the jpg image bacground
+# Assuming each jpg image have corresponding map with _MASK.png ending use the _MASK.png mask image file to delete/mask/dark the jpg image background
 import cv2
 import os
 import numpy
 indir= r"Output_landscape_dataset//" # input folder with subfolders containing images and masks
-outdir="Generated_Dataset_black_background//" # output folder for masks
+outdir="Generated_Dataset_black_background//" # output folder for images without background
+save_mask=True # Do you want to add the mask to the output folder
 if not os.path.exists(outdir): os.mkdir(outdir)
 for sdr in os.listdir(indir):
    outpath=outdir+"//"+sdr
@@ -15,6 +16,8 @@ for sdr in os.listdir(indir):
            if ".jpg" in inpt:
                im=cv2.imread(inpt)
                mask=cv2.imread(inpt.replace(".jpg","_MASK.png"))
-               im[mask<125]=0
-               cv2.imwrite(outpath+"//"+fl,im)
+               im[mask<125]=0 # mask background
+               cv2.imwrite(outpath+"//"+fl,im) # save image
+               if save_mask: # save mask to output dir
+                    cv2.imwrite(outpath+"//"+fl.replace(".jpg","_MASK.png"),mask[:,:,0])
                print(outpath+"//"+fl)
